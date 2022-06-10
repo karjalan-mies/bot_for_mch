2229aa31c99a4b2e4de85917114309a33a8b0940
Sovius <sovius5@mail.ru>
2022-06-10 21:55:10 +0300

    dd static end timezone

--- telega/settings.py
+++ telega/settings.py
@@ -116,6 +116,8 @@ USE_I18N = True
 
 USE_L10N = True
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
