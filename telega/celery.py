22280cf5daccbabdc42e417960778b6daf2cd44f
Sovius <sovius5@mail.ru>
2022-06-11 12:45:41 +0300

    add celery beat

--- telega/celery.py
+++ telega/celery.py
@@ -5,5 +5,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telega.settings')
 
 app = Celery('telega')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.broker_transport_options = {'visibility_timeout': 60}
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
