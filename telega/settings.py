ed56659acd10cb05961efe5097dc06a03062767e
sovius23 <79575666+sovius23@users.noreply.github.com>
2022-06-11 20:33:37 +0300

    Update settings.py

--- telega/settings.py
+++ telega/settings.py
@@ -120,7 +120,7 @@ USE_L10N = True
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
