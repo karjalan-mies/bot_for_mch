ff4a996dd49302c4405c4d88d6a9261b0a802e39
Sovius <sovius5@mail.ru>
2022-06-11 10:11:23 +0300

    add celery beat

--- bot.py
+++ bot.py
@@ -37,8 +37,6 @@ dp = my_bot.dispatcher
 
 from telega.settings import redis_url
 def test(update, context):
-    update.message.reply_text('Тест пройден',
-                              reply_markup=ReplyKeyboardMarkup([['/test']]))
     @shared_task(name="repeat_test")
     def repeat_test():
         update.message.reply_text('Тест пройден',
--- telega/celery.py
+++ telega/celery.py
@@ -4,7 +4,6 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telega.settings')
 
 app = Celery('telega')
-app.config_from_object('django.conf:settings')
-
+app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
